97f1d361c805c4710113bf5892cef68967ae851a
Luca Ghislotti <ghislottiluca@gmail.com>
2022-03-08 11:05:18 +0100

    sistematina al launcher

--- codice_LFC/src/gcodeMain/ParserLauncher.java
+++ codice_LFC/src/gcodeMain/ParserLauncher.java
@@ -4,8 +4,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Collections;
 import org.antlr.runtime.RecognitionException;
-
-import gcodeCompiler.gcodeGrammarLexer;
 import gcodeCompiler.gcodeGrammarParser;
 import gcodeCompiler.util.Error;
 
@@ -13,27 +11,31 @@ public class ParserLauncher {
 	public static void main(String[] args) throws FileNotFoundException, IOException, RecognitionException {
 		String fileIn = ".\\resources\\input.file";
 
-		System.out.println("Parsing con ANTLR\n");
+		System.out.println("GCODE PARSING WITH ANTLR3\n");
 
 		gcodeGrammarParser parser = new gcodeGrammarParser(fileIn);
 
-		
 		parser.gcode();
 
 		if (parser.getErrorList().size() == 0) {
-			System.out.println("Parsing completato con successo\n");
+			System.out.println("Parsing completed successfully with 0 errors\n");
+			System.out.println("Block list");
 			parser.h.printBlocks();
 
 		} else {
 			int i = 0;
-			System.out.println("Errori rilevati");
+
+			if (parser.getErrorList().size() == 1)
+				System.out.println("Parsing failed (1 error)\n");
+			else
+				System.out.println("Parsing failed (" + parser.getErrorList().size() + " errors)\n");
+
+			System.out.println("Error list");
 
 			Collections.sort(parser.getErrorList());
 
 			for (Error errore : parser.getErrorList())
 				System.out.println(++i + " - " + errore.toString());
 		}
-
-		System.out.println("\nParsing Completato");
 	}
 }
