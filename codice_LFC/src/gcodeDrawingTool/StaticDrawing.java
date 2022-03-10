4ae3f28c05c61d47a575c666054848334bffbd48
Luca Ghislotti <ghislottiluca@gmail.com>
2022-03-10 16:22:27 +0100

    Update StaticDrawing.java

--- codice_LFC/src/gcodeDrawingTool/StaticDrawing.java
+++ codice_LFC/src/gcodeDrawingTool/StaticDrawing.java
@@ -73,84 +73,45 @@ public class StaticDrawing extends JFrame {
 
 			// possiamo leggere le coordinate e stamparle di seguito
 
-			for (int i = 2; i <= prova.length - 2; i += 2) {
-				g.drawLine(prova[i - 2], CANVAS_HEIGHT + 50 - prova[i - 1], prova[i],
-						CANVAS_HEIGHT + 50 - prova[i + 1]);
-			}
-
-			g.drawLine(100, 300, 300, 300);
-
-			Token quattrocento = new CommonToken(0);
-			quattrocento.setText("400");
-
-			Token trecento = new CommonToken(0);
-			trecento.setText("300");
-
-			Token duecento = new CommonToken(0);
-			duecento.setText("200");
+			/*
+			 * for (int i = 2; i <= prova.length - 2; i += 2) { g.drawLine(prova[i - 2],
+			 * CANVAS_HEIGHT + 50 - prova[i - 1], prova[i], CANVAS_HEIGHT + 50 - prova[i +
+			 * 1]); }
+			 */
+			g.drawLine(250, CANVAS_HEIGHT - 200, 250, CANVAS_HEIGHT - 50);
 
 			Token moveType = new CommonToken(0);
 			moveType.setText("G02");
 
-			Coordinate puntoFinale = new Coordinate(quattrocento, quattrocento, null);
-			Coordinate puntoCentrale = new Coordinate(quattrocento, trecento, null);
+			Token X = new CommonToken(0);
+			X.setText("300");
+
+			Token Y = new CommonToken(0);
+			Y.setText("250");
+
+			Token I = new CommonToken(0);
+			I.setText("300");
+
+			Token J = new CommonToken(0);
+			J.setText("200");
+
+			Coordinate puntoFinale = new Coordinate(X, Y, null);
+			Coordinate puntoCentrale = new Coordinate(I, J, null);
 			CircularMove cm = new CircularMove(moveType, puntoFinale, puntoCentrale);
 
-			// g.drawArc(300 - 100, 300, 100 + 100, 100 + 100, 0, 90);
-
-			/*
-			 * TODO funzione per disegnare l'arco in GCODE che usi però la struttura del
-			 * drawArc()
-			 */
-			// circularInterpolationMAN(cm, g);
-
-			circularInterpolation(new Coordinate(trecento, trecento, null), cm, g);
-
+			circularInterpolation(cm, g);
 		}
 
-		// versione manuale della conversione interpolazione circolare
-		private void circularInterpolationMAN(CircularMove cm, Graphics g) {
-			int raggio1 = Math.abs(cm.getC_xyz().getFirst() - cm.getC_ijk().getFirst());
-			int raggio2 = Math.abs(cm.getC_xyz().getSecond() - cm.getC_ijk().getSecond());
-
-			int raggio = raggio2;
-
-			int xTopLeft = cm.getC_ijk().getFirst() - raggio;
-			int yTopLeft = cm.getC_ijk().getSecond() - raggio;
-
-			int width = raggio * 2;
-			int height = raggio * 2;
-
-			int startAngle = -180;
-			int arcAngle = -90;
-
-			System.out.println("\nraggio: " + raggio);
-			System.out.println("xTopLeft: " + xTopLeft);
-			System.out.println("yTopLeft: " + yTopLeft);
-			System.out.println("width = height: " + width);
-			System.out.println("startAngle: " + startAngle);
-			System.out.println("arcAngle: " + arcAngle);
-
-			// alla fine disegno usando il metodo della libreria
-			g.drawArc(xTopLeft, yTopLeft, width, height, startAngle, arcAngle);
-		}
-
-		// versione definitiva dell'interpolazione circolare (solo 90 gradi)
-		private void circularInterpolation(Coordinate pt_prev, CircularMove cm, Graphics g) {
+		// versione definitiva dell'interpolazione circolare
+		private void circularInterpolation(CircularMove cm, Graphics g) {
 			int raggio1 = Math.abs(cm.getC_xyz().getFirst() - cm.getC_ijk().getFirst());
 			int raggio2 = Math.abs(cm.getC_xyz().getSecond() - cm.getC_ijk().getSecond());
 			int raggio = 0;
-
-			int xTopLeft = 0;
-			int yTopLeft = 0;
-
 			int startAngle = 0;
 			int arcAngle = 0;
 
 			if (cm.getC_xyz().getFirst() == cm.getC_ijk().getFirst()) {
 				raggio = raggio2;
-				xTopLeft = cm.getC_ijk().getFirst() - raggio;
-				yTopLeft = cm.getC_ijk().getSecond() - raggio;
 
 				if (cm.getC_xyz().getSecond() > cm.getC_ijk().getSecond()) {
 					if (cm.getMoveType().equals("G02")) { // 1
@@ -161,12 +122,12 @@ public class StaticDrawing extends JFrame {
 						arcAngle = 90;
 					}
 				} else if (cm.getC_xyz().getSecond() < cm.getC_ijk().getSecond()) {
-					if (cm.getMoveType().equals("G02")) { // 4
-						startAngle = 0;
-						arcAngle = -90;
-					} else { // 5
+					if (cm.getMoveType().equals("G03")) { // 4
 						startAngle = -180;
 						arcAngle = 90;
+					} else { // 5
+						startAngle = 0;
+						arcAngle = -90;
 					}
 				}
 			}
@@ -175,13 +136,9 @@ public class StaticDrawing extends JFrame {
 				raggio = raggio1;
 				if (cm.getC_xyz().getFirst() > cm.getC_ijk().getFirst()) {
 					if (cm.getMoveType().equals("G03")) { // 2
-						xTopLeft = cm.getC_ijk().getFirst() - raggio;
-						yTopLeft = cm.getC_ijk().getSecond() - raggio * 3;
 						startAngle = -90;
 						arcAngle = 90;
 					} else { // 3
-						xTopLeft = cm.getC_ijk().getFirst() - raggio;
-						yTopLeft = cm.getC_ijk().getSecond() + raggio;
 						startAngle = 90;
 						arcAngle = -90;
 					}
@@ -189,15 +146,11 @@ public class StaticDrawing extends JFrame {
 
 				if (cm.getC_ijk().getFirst() > cm.getC_xyz().getFirst()) {
 					if (cm.getMoveType().equals("G03")) { // 6
-						xTopLeft = cm.getC_ijk().getFirst() - raggio;
-						yTopLeft = cm.getC_ijk().getSecond() + raggio;
 						startAngle = 90;
 						arcAngle = 90;
 					} else { // 7
-						xTopLeft = cm.getC_ijk().getFirst() - raggio;
-						yTopLeft = cm.getC_ijk().getSecond() - raggio * 3;
 						startAngle = -180;
-						arcAngle = -90;
+						arcAngle = 90;
 					}
 				}
 			}
@@ -205,12 +158,8 @@ public class StaticDrawing extends JFrame {
 			int width = raggio * 2;
 			int height = raggio * 2;
 
-			System.out.println("\nraggio: " + raggio);
-			System.out.println("xTopLeft: " + xTopLeft);
-			System.out.println("yTopLeft: " + yTopLeft);
-			System.out.println("width = height: " + width);
-			System.out.println("startAngle: " + startAngle);
-			System.out.println("arcAngle: " + arcAngle);
+			int xTopLeft = cm.getC_ijk().getFirst() - raggio;
+			int yTopLeft = CANVAS_HEIGHT - cm.getC_ijk().getSecond() - raggio;
 
 			// alla fine disegno usando il metodo della libreria
 			g.drawArc(xTopLeft, yTopLeft, width, height, startAngle, arcAngle);
